3393ac4271ed4842b6fb03e5cd049da9efc4b888
tarox <parkjh1983@gmail.com>
2022-12-15 00:27:16 +0900

    1215 homecommit, 강의 3-14 06:45. 뭔가 정상적으로 되고 있지 않음.

--- src/main/java/com/sparta/week03/Week03Application.java
+++ src/main/java/com/sparta/week03/Week03Application.java
@@ -2,7 +2,9 @@ package com.sparta.week03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // 생성시각, 수정시각 반영
 @SpringBootApplication
 public class Week03Application {
 
--- src/main/java/com/sparta/week03/domain/TimeStamped.java
+++ src/main/java/com/sparta/week03/domain/TimeStamped.java
@@ -2,6 +2,7 @@ package com.sparta.week03.domain;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
@@ -10,7 +11,8 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-@MappedSuperclass
+@MappedSuperclass // Entity가 자동으로 컬럼으로 인식합니다.
+@Getter
 @EntityListeners(AuditingEntityListener.class)
 public abstract class TimeStamped {
     @CreatedDate
